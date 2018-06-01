45bd5f9d83690ea552eae8dc5011d6990e9c41d0
alcander_liu <chiawei.liu.1201@gmail.com>
2018-06-02 01:01:59 +0800

    add taskmgr.c and some function frames

---
+++ taskmgr.c
@@ -0,0 +1,33 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+void getCommand()
+{
+
+}
+
+void printTaskMgrInfo()
+{
+
+  while(1)
+  {
+    getCommand();
+  }
+}
+
+void printString()
+{
+
+}
+
+void printPageInfo()
+{
+
+}
+
+int main()
+{
+  printMgrInfo();
+  return 0;
+}
