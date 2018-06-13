cce26217a69d227a37daf9781bb1d251fd8063cf
Guochi Gu <3516336273@qq.com>
2018-06-13 11:27:25 -0700

    Complete ininTaskMgrInfo(), clearTaskMgrInfo() and updTaskMgrInfo()

--- taskmgr.c
+++ taskmgr.c
@@ -8,9 +8,11 @@
 #define KEY_LF 0xE4
 #define KEY_RT 0xE5
 
-const char procstate[6][9] = {"UNUSED", "EMBRYO", "SLEEPING", "RUNNABLE", "RUNNING", "ZOMBIE"};
+char procstate[6][9] = {"UNUSED", "EMBRYO", "SLEEPING", "RUNNABLE", "RUNNING", "ZOMBIE"};
+char digits[] = "0123456789";
+char blankprocinfo[] = "0                   0                   0                   0";
 
-char buff[96][80];
+char scrbuff[96][80];
 int curline = 4;  //表示当前高亮显示的行序号，范围4-19
 int curpage = 0;  //表示当前页码，范围0-3
 int procID[NPROC];
@@ -22,12 +24,9 @@ void runTaskMgr(void);
 int getCmd(char *buf);
 void procCmd(char *cmd);
 //int fork2(void);
-void initTaskMgrInfo();
-void updateTaskMgrInfo(void);
-//void printTaskMgrInfo(void);
-//void printString(char *fmt, int line, ...);
-//void printPageInfo();
-//int printint(int xx, int base, int sign, int pos, int limit, char *buff);
+void initTaskMgrInfo(void);
+void clearTaskMgrInfo(void);
+void updTaskMgrInfo(void);
 
 void
 runTaskMgr(void)
@@ -36,8 +35,8 @@ runTaskMgr(void)
   char *c;
 
   initTaskMgrInfo();
-  updateTaskMgrInfo();
-  updscrcont(buff[curpage*24], curline);
+  updTaskMgrInfo();
+  updscrcont(scrbuff[curpage*24], curline);
   
   while(getCmd(buf) >= 0)
   {
@@ -51,8 +50,8 @@ runTaskMgr(void)
     */
     c = buf;
     procCmd(c);
-    updateTaskMgrInfo();
-    updscrcont(buff[curpage*24], curline);
+    updTaskMgrInfo();
+    updscrcont(scrbuff[curpage*24], curline);
   }
 }
 
@@ -121,227 +120,72 @@ fork2(void)
 void
 initTaskMgrInfo(void)
 {
-  char title1[] = "Task Maneger";
-  char title2[] = "pid        name       state      memory(bytes)";
+  char title1[] = "Task Manager";
+  char title2[] = "pid                 name                state               memory(bytes)";
   char footNote[] = "page: ";
-  int curDigit;
-  for(int page = 0; page < 4; page++)
-  {
-    curDigit = 0;
-    for(char *s = title1; *s != '\0'; s++)
-    {
-      buff[24*page + 0][curDigit++] = *s;
-    }
-    curDigit = 0;
-    for(char *s = title2; *s != '\0'; s++)
-    {
-      buff[24*page + 1][curDigit++] = *s;
-    }
-    curDigit = 40;
-    for(char *s = footNote; *s != '\0'; s++)
-    {
-      buff[24*page + 23][curDigit++] = *s;
-    }
-    buff[24*page + 23][curDigit++] = page + '1';
-    buff[24*page + 23][curDigit++] = '/';
-    buff[24*page + 23][curDigit++] = '4';
-  }
-}
-
-void
-updateTaskMgrInfo(void)
-{
-  int curDigit;
-  char localBuff[10];
-  static char digits[] = "0123456789";
-  getprocinfo(procID, procName, procState, procSize);
-  for(int page = 0; page < 4; page++)
-  {
-    for(int i = 0; i < 16; i++)
-    {
-      curDigit = 0;
-      int localPartDigit = 0;
-      do
-      {
-        localBuff[localPartDigit++] = digits[procID[24*page + i] % 10];
-      } while((procID[24*page + i] /= 10) != 0 && localPartDigit < 10);
-      while(localPartDigit > 0)
-      {
-        buff[24*page + (i+4)][curDigit++] = localBuff[--localPartDigit]; 
-      }
-      while(curDigit < 11)
-      {
-        buff[24*page + (i+4)][curDigit++] = 0;
-      }
-
-      localPartDigit = 0;
-      if(procName[24*page + i][0] == 0)
-      {
-        buff[24*page + (i+4)][curDigit++] = '0';
-      }
-      else
-      {
-        while(procName[24*page + i][localPartDigit] && curDigit < 21)
-        {
-          buff[24*page + (i+4)][curDigit++] = procName[24*page + i][localPartDigit++];
-        }
-      }
-      while(curDigit < 22)
-      {
-        buff[24*page + (i+4)][curDigit++] = 0;
-      }
-
-      localPartDigit = 0;
-      while(procstate[procState[24*page + i]][localPartDigit] != '\0' && curDigit < 32)
-      {
-        buff[24*page + (i+4)][curDigit++] = procstate[procState[24*page + i]][localPartDigit++];
-      }
-      while(curDigit < 33)
-      {
-        buff[24*page + (i+4)][curDigit++] = 0; 
-      }
-
-      localPartDigit = 0;
-      do
-      {
-        localBuff[localPartDigit++] = digits[procSize[24*page + i] % 10];
-      } while((procSize[24*page + i] /= 10) != 0 && localPartDigit < 10);
-      while(localPartDigit > 0)
-      {
-        buff[24*page + (i+4)][curDigit++] = localBuff[--localPartDigit]; 
-      }
-      while(curDigit < 44)
-      {
-        buff[24*page + (i+4)][curDigit++] = 0;
-      }
-    }
-  }
-}
-
-/*
-void
-printTaskMgrInfo(void)
-{
-  printf(1, "printTaskMgrInfo\n");
-}
-
-void
-printString(char *fmt, int line, ...) // 打印在第line行
-{
-  int c, digitsDesired, digitsUsed, remain, currentDigit = 0; //目前輸出到第line行第currentDigit位
+  int page, row, col;
   char *s;
-  memset(buff[line/80], 0, sizeof(char)*80);
-  uint *argp;
-  
-  if (fmt == 0)
-  {
-    printf(1, "null fmt\n");
-  }
-  
-  for(int i = 0; (c = fmt[i] & 0xff) != 0; i++)
-  {
-    if (currentDigit = 0)
-    {
-      goto bad;
-    }
-    if (c != '%')
-    {
-      if (c == '\n')
-      {
-        buff[line/80][currentDigit++] = 0;
-        currentDigit = 80;
-      }
-      else
-      {
-        buff[line/80][currentDigit++] = c;
-      }
-      continue;
-    }
-    c = fmt[++i] & 0xff;
-    if (c == 0)
-    {
-      break;
-    }
-    digitsDesired = 0;
-    digitsUsed = 0;
-    while (c >= '0' && c <= '9')
-    {
-      digitsDesired = digitsDesired * 10 + c - '0';
-      c = fmt[++i] & 0xff;
-    }
-    if (currentDigit + digitsDesired > 80)
-    {
-      goto bad;
-    }
-    remain = (digitsDesired == 0) ? (80 - currentDigit) : digitsDesired;
-    
-    switch(c)
-    {
-      case 'd':
-        digitsUsed = printint(*argp++, 10, 1, currentDigit, remain, buff[line / 80]);
-        currentDigit += digitsUsed;
-        while(digitsUsed <= digitsDesired)
-        {
-          buff[line / 80][currentDigit++] = 0;
-          digitsUsed++;
-        }
-        break;
-      case 'x':
-      case 'p':
-        digitsUsed = printint(*argp++, 16, 0, currentDigit, remain, buff[line / 80]);
-        currentDigit += digitsUsed;
-        while(digitsUsed <= digitsDesired)
-        {
-          buff[line / 80][currentDigit++] = 0;
-          digitsUsed++;
-        }
-        break;
-      case 's':
-        if((s = (char*)*argp++) == 0)
-    	    s = "(null)";
-        while(digitsUsed < remain)
-        {
-          buff[line / 80][currentDigit++] = *s;
-          s++;
-          digitsUsed++;
-        }
-	  		while(digitsUsed <= digitsDesired) //補空格
-	  		{
-				  buff[line / 80][currentDigit++] = 0;
-				  digitsUsed++;
-	  		}
-        break;
-      case '%':
-        buff[line / 80][currentDigit] = c;
-        break;
-      default:
-        if (currentDigit > 78)
-        {
-          goto bad;
-        }
-        else
-        {
-          buff[line / 80][currentDigit++] = '%';
-          buff[line / 80][currentDigit++] = c;
-        }
-        break;
-    }
-    return;
-  }
-  bad:
-}
 
-int printint(int xx, int base, int sign, int pos, int limit, char *buff)
-{
+  for(page = 0; page < 4; page++)
+    for(row = 0; row < 24; row++)
+      for(col = 0; col < 80; col++)
+        scrbuff[24*page+row][col] = '\0';
 
+  for(page = 0; page < 4; page++){
+    for(col = 34, s = title1; *s != '\0'; s++)
+      scrbuff[24*page][col++] = *s;
+    for(col = 0, s = title2; *s != '\0'; s++)
+      scrbuff[24*page+2][col++] = *s;
+    for(col = 70, s = footNote; *s != '\0'; s++)
+      scrbuff[24*page+23][col++] = *s;
+    scrbuff[24*page+23][col++] = page+'1';
+    scrbuff[24*page+23][col++] = '/';
+    scrbuff[24*page+23][col++] = '4';
+  }
 }
 
 void
-printPageInfo()
+clearTaskMgrInfo(void)
 {
+  int page, row, col;
 
+  for(page = 0; page < 4; page++)
+    for(row = 4; row < 20; row++)
+      for(col = 0; col < 80; col++)
+        scrbuff[24*page+row][col] = '\0';
 }
-*/
+
+void
+updTaskMgrInfo(void)
+{
+  int page, row, col, x, i;
+  char *s;
+  int localbuff[20];
+  
+  getprocinfo(procID, procName, procState, procSize);
+  for(page = 0; page < 4; page++)
+    for(row = 4; row < 20; row++)
+      if(procID[16*page+row-4] <= 0)
+        for(col = 0, s = blankprocinfo; *s != '\0'; s++)
+          scrbuff[24*page+row][col++] = *s;
+      else{
+        for(i = -1, x = procID[16*page+row-4]; x > 0; x/=10)
+          localbuff[++i] = x%10;
+        for(col = 0; i >= 0; i--)
+          scrbuff[24*page+row][col++] = digits[localbuff[i]];
+
+        for(col = 20, s = procName[16*page+row-4]; *s != '\0'; s++)
+          scrbuff[24*page+row][col++] = *s;
+
+        for(col = 40, s = procstate[procState[16*page+row-4]]; *s != '\0'; s++)
+          scrbuff[24*page+row][col++] = *s;
+
+        for(i = -1, x = procSize[16*page+row-4]; x > 0; x/=10)
+          localbuff[++i] = x%10;
+        for(col = 60; i >= 0; i--)
+          scrbuff[24*page+row][col++] = digits[localbuff[i]];
+      }
+}
 
 int
 main(void)
